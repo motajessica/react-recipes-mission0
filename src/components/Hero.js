ebdba266608c5f93fab068438c71e5f9c2c331e7
motajessica <motajessicaa@gmail.com>
2023-04-25 12:07:07 +1200

    Fix hero page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,44 +3,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Hero() {
   return (
-    <div
-      className="img-hero mb-5 p-5 text-center bg-image"
-      style={{ backgroundImage: "url('/images/cooking-1.jpg')", height: 400 }}>
+    <div className="bg-warning">
+
+      <div class="container p-5 mb-5">
+        <div class="row">
+          <div class="col-12 col-md-6">
+              <div class="text-white text-center mb-5">
+
+                <div className="py-3">
+                  <h2>Unleash your chef inner with our delicious recipes!</h2>
+                </div>
+
+                <div>
+                  <button type="button" class="btn btn-dark btn-lg download-button mb-3">
+                    <i class="fab fa-apple me-1"></i>Download
+                  </button>
+                  <button type="button" class="btn btn-outline-light btn-lg download-button mx-2 mb-3">
+                      <i class="fab fa-google-play me-1"></i>Download
+                  </button>
+                </div>  
+
+              </div>
+          </div>
       
-      <div className="row">
-        <div className="col-lg-6 mb-5">
-          <h2 className="text-white mb-3">
-            Unleash your inner chef with our delicious recipes.
-          </h2>
-          <button
-            type="button"
-            class="btn btn-dark btn-lg download-button mb-3"
-          >
-            <i class="fab fa-apple me-1"></i>Download
-          </button>
-          <button
-            type="button"
-            class="btn btn-outline-light btn-lg download-button mb-3"
-          >
-            <i class="fab fa-google-play me-1"></i>Download
-          </button>
+          <div className="col-12 col-md-6" >
+            <div class="text-center">
+              <img class="welcome-img" src="/logo192.png" alt="..."></img>
+            </div>
+          </div>
         </div>
 
-        <div className="col-lg-6 position-relative"></div>
-
-        <div className="search mb-5">
-          <input
-            className="mb-2 mx-2 mr-sm-2 "
-            type="search"
-            placeholder="Search"
-            onChange={() => {}}
-          />
-          <button type="submit" className="btn my-2 my-sm-0 btn-sm btn-warning">
-            Ok
-          </button>
-        </div>
+        <div>
+         
       </div>
+
+        </div>
+
     </div>
+    
   );
 }
 
